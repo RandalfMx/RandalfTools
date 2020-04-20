3ae0b66bee82931407f98010c9e617c5d9165184
Massimiliano Randazzo <massimiliano.randazzo@gmail.com>
2020-04-20 09:38:53 +0200

    Aggiunto alcune procedure di test

---
+++ src/test/java/mx/randalf/tools/ChecksumTest.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package mx.randalf.tools;
+
+import java.security.NoSuchAlgorithmException;
+
+
+/**
+ * @author massi
+ *
+ */
+public class ChecksumTest {
+
+	/**
+	 * 
+	 */
+	public ChecksumTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		MD5Tools md5 = null;
+		SHA1Tools sha1 = null;
+		SHA256Tools sha256 = null;
+		String parola = "";
+		String value = "";
+		
+		try {
+			
+			parola = "welcome";
+			
+			value = MD5Tools.checkSum(parola.getBytes());
+			System.out.println("MD5:\t\t"+value.length()+" -> "+value);
+			
+			value = SHA1Tools.checkSum(parola.getBytes());
+			System.out.println("SHA1:\t\t"+value.length()+" -> "+value);
+			
+			sha256 = new SHA256Tools();
+			value = sha256.checkSum(parola.getBytes());
+			System.out.println("SHA256:\t\t"+value.length()+" -> "+value);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
